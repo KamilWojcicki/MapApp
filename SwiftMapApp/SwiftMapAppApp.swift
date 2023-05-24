be759cf56394dff8eea2b3b59a6f75a8d41b7528
Kamil Wojcicki <kamilwojcickinjr@gmail.com>
2023-05-24 15:06:41 +0200

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Kamil Wójcicki on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
